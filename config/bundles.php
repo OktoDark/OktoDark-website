9427a906df6a4d6841d92e80925bb707751cb873
Viruzzz <mvt3am@yahoo.com>
2018-03-12 18:32:32 +0200

    Enable new bundles in config file

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     EasyCorp\Bundle\EasyDeployBundle\EasyDeployBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
