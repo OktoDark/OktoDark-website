cbabf80b3e838fc1582aace0133aaed5797a3960
Viruzzz <mvt3am@yahoo.com>
2018-07-15 12:56:39 +0300

    Reworked Contact form

--- src/Controller/InfoController.php
+++ src/Controller/InfoController.php
@@ -5,25 +5,20 @@
  *
  * Author: Razvan George H. (Viruzzz)
  *
- * File date of modification: 09.05.2018 12:20
+ * File date of modification: 15.07.2018 12:55
  */
 
 namespace App\Controller;
 
+use App\Form\ContactType;
 use App\Repository\SettingsRepository;
 use App\Repository\TeamRepository;
-use Doctrine\DBAL\Connection;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class InfoController extends Controller
+class InfoController extends AbstractController
 {
     /**
      * @Route("/about", methods={"GET"}, name="about")
@@ -39,36 +34,36 @@ class InfoController extends Controller
     }
 
     /**
-     * @Route("/contact", methods={"GET"}, name="contact")
-     *
-     * @return Response
+     * @Route("/contact", name="contact")
      */
     public function contact(SettingsRepository $settings, Request $request, \Swift_Mailer $mailer): Response
     {
         $selectSettings = $settings->findAll();
 
-        $contact = ['message' => 'Type your message'];
-
-        $form = $this->createFormBuilder($contact)
-            ->add('name', TextType::class)
-            ->add('email', EmailType::class)
-            ->add('subject', TextType::class)
-            ->add('message', TextareaType::class)
-            ->add('submit', SubmitType::class)
-            ->getForm();
+        $form = $this->createForm(ContactType::class);
 
         $form->handleRequest($request);
 
+        $this->addFlash('info', 'Do not spam');
+
         if ($form->isSubmitted() && $form->isValid())
         {
-            $message = (new \Swift_Mailer('.$name.'))
-                ->setFrom('..')
+            $contactFormData = $form->getData();
+
+            $message = (new \Swift_Message())
+                ->setFrom($contactFormData['email'])
                 ->setTo('')
-                ->setBody('')
+                ->setBody(
+                    $contactFormData['message'],
+                    'text/plain'
+                )
                 ;
 
             $mailer->send($message);
-            $contact = $form->getData();
+
+            $this->addFlash('success', 'Message Send');
+
+            return $this->redirectToRoute('contact');
         }
 
         return $this->render('@theme/info/contact.html.twig', ['settings' => $selectSettings, 'form' => $form->createView()]);
---
+++ src/Form/ContactType.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Copyright (c) 2018 OktoDark Studios
+ * Website: https://www.oktodark.com
+ *
+ * Author: Razvan George H. (Viruzzz)
+ *
+ * File date of modification: 15.07.2018 12:55
+ */
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('subject', TextType::class)
+            ->add('message', TextareaType::class)
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
