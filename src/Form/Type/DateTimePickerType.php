c432f49f8c2b0fbbd2b0c75e5d08583c2749270f
Viruzzz <mvt3am@yahoo.com>
2018-10-19 20:20:19 +0300

    Fixed: DateTimePicker

--- src/Form/Type/DateTimePickerType.php
+++ src/Form/Type/DateTimePickerType.php
@@ -34,6 +34,9 @@ class DateTimePickerType extends AbstractType
     {
         $resolver->setDefaults([
             'widget' => 'single_text',
+            // if true, the browser will display the native date picker widget
+            // however, this app uses a custom JavaScript widget, so it must be set to false
+            'html5' => false,
         ]);
     }
 
