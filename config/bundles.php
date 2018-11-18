90ace39e6458bfd7e865f4a1e6e9a2e5291775a2
Viruzzz <mvt3am@yahoo.com>
2018-11-18 13:45:20 +0200

    Adding: webpack encore to project

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
