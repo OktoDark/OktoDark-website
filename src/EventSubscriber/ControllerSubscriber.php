7108f16d57f3638a13810989852c3a0f07addfa6
Viruzzz <mvt3am@yahoo.com>
2019-03-16 17:28:40 +0200

    Adding: missing files

---
+++ src/EventSubscriber/ControllerSubscriber.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Copyright (c) 2018 OktoDark Studios
+ * Website: https://www.oktodark.com
+ *
+ * Author: Razvan George H. (Viruzzz)
+ *
+ * File date of modification: 16.03.2019 17:25
+ */
+
+namespace App\EventSubscriber;
+
+use App\Twig\SourceCodeExtension;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Defines the method that 'listens' to the 'kernel.controller' event, which is
+ * triggered whenever a controller is executed in the application.
+ */
+class ControllerSubscriber implements EventSubscriberInterface
+{
+    private $twigExtension;
+
+    public function __construct(SourceCodeExtension $twigExtension)
+    {
+        $this->twigExtension = $twigExtension;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::CONTROLLER => 'registerCurrentController',
+        ];
+    }
+
+    public function registerCurrentController(FilterControllerEvent $event): void
+    {
+        // this check is needed because in Symfony a request can perform any
+        // number of sub-requests. See
+        // https://symfony.com/doc/current/components/http_kernel/introduction.html#sub-requests
+        if ($event->isMasterRequest()) {
+            $this->twigExtension->setController($event->getController());
+        }
+    }
+}
