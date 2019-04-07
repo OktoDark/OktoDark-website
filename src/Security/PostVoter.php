5bb647c8b08e0a02de13072809d6f07e684b2134
Viruzzz <mvt3am@yahoo.com>
2019-04-07 03:18:58 +0300

    Added missing file for last commit

---
+++ src/Security/PostVoter.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (c) 2019 OktoDark Studios
+ * Website: https://www.oktodark.com
+ *
+ * Author: Razvan George H. (Viruzzz)
+ *
+ * File date of modification: 07.04.2019 03:04
+ */
+
+namespace App\Security;
+
+use App\Entity\Post;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class PostVoter extends Voter
+{
+    // Defining these constants is overkill for this simple application, but for real
+    // applications, it's a recommended practice to avoid relying on "magic strings"
+    public const DELETE = 'delete';
+    public const EDIT = 'edit';
+    public const SHOW = 'show';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports($attribute, $subject): bool
+    {
+        // this voter is only executed for three specific permissions on Post objects
+        return $subject instanceof Post && \in_array($attribute, [self::SHOW, self::EDIT, self::DELETE], true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function voteOnAttribute($attribute, $post, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        // the user must be logged in; if not, deny permission
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        // the logic of this voter is pretty simple: if the logged user is the
+        // author of the given blog post, grant permission; otherwise, deny it.
+        // (the supports() method guarantees that $post is a Post object)
+        return $user === $post->getAuthor();
+    }
+}
