fcd2235150a97e2427fa37413ea05e10e5da1d66
Viruzzz <mvt3am@yahoo.com>
2020-06-13 00:16:47 +0300

    removed some lines from server_start.bat  to start more faster the server

--- server_start.bat
+++ server_start.bat
@@ -1,11 +1,6 @@
 echo off
 cls
 title OktoDark Studios Website
-echo The site is starting soon ...
-echo Please Wait!
-TIMEOUT 5 >nul
-echo Loading ...
-TIMEOUT 5 >nul
-echo Loading ... Complete
+echo The website has been started
 php -S 127.0.0.1:8000 -t public/
 pause
