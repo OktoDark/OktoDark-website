46f1729f716475b9f83569b509b8b9c78d1f70de
Viruzzz <mvt3am@yahoo.com>
2022-05-19 11:00:54 +0300

    Fixed Lint from last commit

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -17,7 +17,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Defines the properties of the Comment entity to represent the blog comments.
- * See https://symfony.com/doc/current/book/doctrine.html#creating-an-entity-class
+ * See https://symfony.com/doc/current/book/doctrine.html#creating-an-entity-class.
  *
  * Tip: if you have an existing database, you can generate these entity class automatically.
  * See https://symfony.com/doc/current/cookbook/doctrine/reverse_engineering.html
