44ec05aba72303aea4c4c5b40041d9306a1b9da6
Viruzzz <mvt3am@yahoo.com>
2023-01-24 11:00:12 +0200

    Update: Maintenance system

---
+++ src/EventSubscriber/KernelRequestSubscriber.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ * Copyright (c) OktoDark Studios
+ * Website: https://www.oktodark.com
+ *
+ * Author: Razvan George H. (Viruzzz)
+ *
+ * For the full copyright and license information, please view the LICENSE.
+ */
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Environment as TwigEnvironment;
+
+class KernelRequestSubscriber implements EventSubscriberInterface
+{
+    private TwigEnvironment $twig;
+
+    public function __construct(TwigEnvironment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => [
+                ['onMaintenance', \PHP_INT_MAX - 1000],
+            ],
+        ];
+    }
+
+    public function onMaintenance(RequestEvent $event): void
+    {
+        /** @var bool $isMaintenance */
+        $isMaintenance = \filter_var($_ENV['MAINTENANCE_MODE'] ?? '0', \FILTER_VALIDATE_BOOLEAN);
+        $isCli = \PHP_SAPI === 'cli';
+
+        if ($isMaintenance && !$isCli) {
+            $event->setResponse(new Response(
+                $this->twig->render('@theme/maintenance/maintenance.html.twig'),
+                Response::HTTP_SERVICE_UNAVAILABLE,
+            ));
+            $event->stopPropagation();
+        }
+    }
+}
