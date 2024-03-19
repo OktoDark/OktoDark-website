82309a57d5c72f9733ee70f261df63db70f82b7c
Viruzzz <mvt3am@yahoo.com>
2024-03-19 10:51:53 +0200

    Fixing some CS Lint

--- src/Repository/PostRepository.php
+++ src/Repository/PostRepository.php
@@ -36,7 +36,7 @@ class PostRepository extends ServiceEntityRepository
         parent::__construct($registry, Post::class);
     }
 
-    public function findLatest(int $page = 1, Tag $tag = null): Paginator
+    public function findLatest(int $page = 1, ?Tag $tag = null): Paginator
     {
         $qb = $this->createQueryBuilder('p')
             ->addSelect('a', 't')
