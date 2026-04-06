df048133094d1a7322ae3cd00bf5042ae3e36d0d
Viruzzz <mvt3am@yahoo.com>
2026-04-06 16:16:54 +0300

    Lint Fixed

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -189,10 +189,10 @@ final class BlogController extends AbstractController
 
         $results = array_map(function (Post $post) {
             return [
-                'title' => htmlspecialchars($post->getTitle(), \ENT_COMPAT | ENT_HTML5),
+                'title' => htmlspecialchars($post->getTitle(), \ENT_COMPAT | \ENT_HTML5),
                 'date' => $post->getPublishedAt()->format('M d, Y'),
-                'author' => htmlspecialchars($post->getAuthor()->getFullName(), \ENT_COMPAT | ENT_HTML5),
-                'summary' => htmlspecialchars($post->getSummary(), \ENT_COMPAT | ENT_HTML5),
+                'author' => htmlspecialchars($post->getAuthor()->getFullName(), \ENT_COMPAT | \ENT_HTML5),
+                'summary' => htmlspecialchars($post->getSummary(), \ENT_COMPAT | \ENT_HTML5),
                 'url' => $this->generateUrl('blog_post', ['slug' => $post->getSlug()]),
             ];
         }, $foundPosts);
