cc8b2aa64bfc8e4721d97585bb171bdb91fbe6da
Viruzzz <mvt3am@yahoo.com>
2026-04-20 08:44:32 +0300

    Yarn and Vite updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import symfonyPlugin from "vite-plugin-symfony";
 export default defineConfig({
     plugins: [
         symfonyPlugin(),
-    ],symfony serve
+    ],
     build: {
         rollupOptions: {
             input: {
