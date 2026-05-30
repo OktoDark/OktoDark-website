b9550983a4ea9dcb21e917c3e40bd4cc813843f9
Viruzzz <mvt3am@yahoo.com>
2026-05-30 11:13:02 +0300

    typo

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -12,10 +12,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Intl\Countries;
 
